18c1326ee0765a15170562bfee4ffff5a6f25db2
Se Won Jang <swjang@stanford.edu>
2013-09-11 08:42:46 -0700

    add returning scroll

--- app/assets/javascripts/deimos_scroll.js
+++ app/assets/javascripts/deimos_scroll.js
@@ -12,7 +12,7 @@ Deimos_Scroll.prototype.scrollTo = function(position) {
 
 Deimos_Scroll.prototype.scrollTo_Element = function(elementName) {
 	console.log($.inArray(elementName, this.elemNames));
-	return this.scrollTo(elemOffsets[$.inArray(elementName, this.elemNames)]);
+	return this.scrollTo(this.elemOffsets[$.inArray(elementName, this.elemNames)]);
 }
 
 Deimos_Scroll.prototype.setOnScrollListener = function() {
@@ -21,10 +21,10 @@ Deimos_Scroll.prototype.setOnScrollListener = function() {
 		
 	$(window).on('scroll', function() {
 		if (obj.isScrolling == false) {
-			obj.scroll = $(window).scrollTop(),
-				scroll_change = obj.scroll - this.previous_scroll;
+			obj.scroll = $(window).scrollTop();
+			obj.scroll_change = obj.scroll - this.previous_scroll;
 			this.previous_scroll = obj.scroll;
-			$(window).trigger('custom_scroll', [scroll_change]);
+			$(window).trigger('custom_scroll', [obj.scroll_change]);
 		}
 	});
 
@@ -37,31 +37,31 @@ Deimos_Scroll.prototype.setOnScrollListener = function() {
 	$(window).scroll(function() {
 		clearTimeout($.data(this, 'scrollTimer'));
 		$.data(this, 'scrollTimer', setTimeout(function() {
-			// do something
+			obj.getScrollDest();
 			console.log("Haven't scrolled in 250ms!");
 		}, 200));
 	});
 }
 
 Deimos_Scroll.prototype.getScrollDest = function(scroll_change) {
-	if (this.previous_scroll <= 1000) {
+	if (this.previous_scroll <= 2000) {
 		if (scroll_change > 0) {
-			this.scrollTo(1000);
+			this.scrollTo(2000);
 		} else {
 			this.scrollTo(0);
 		}
-	} else if (this.previous_scroll <= 2000 && this.previous_scroll >= 1000) {
+	} else if (this.previous_scroll <= 4000 && this.previous_scroll >= 2000) {
 		if (scroll_change > 0) {
-			this.scrollTo(2000);
+			this.scrollTo(4000);
 		} else {
 			this.scrollTo(1000);
 		}
 
-	} else if (this.previous_scroll <= 3000 && this.previous_scroll >= 2000) {
+	} else if (this.previous_scroll <= 6000 && this.previous_scroll >= 4000) {
 		if (scroll_change > 0) {
-			this.scrollTo(3000);
+			this.scrollTo(6000);
 		} else {
-			this.scrollTo(2000);
+			this.scrollTo(4000);
 		}
 	}
 	this.isScrolling = true;
