db9635737b698c7c2be410e65e21ead6b6ad87f2
Se Won Jang <swjang@stanford.edu>
2013-09-11 02:47:00 -0700

    scroll fixes

--- app/assets/javascripts/deimos_scroll.js
+++ app/assets/javascripts/deimos_scroll.js
@@ -11,20 +11,44 @@ Deimos_Scroll.prototype.scrollTo = function(position) {
 }
 
 Deimos_Scroll.prototype.scrollTo_Element = function(elementName) {
-	return this.scrollTo(elemOffsets[$.inArray(elementName, elemNames)]);
+	return this.scrollTo(elemOffsets[$.inArray(elementName, this.elemNames)]);
 }
 
 Deimos_Scroll.prototype.setOnScrollListener = function() {
-	var previous_scroll = $(window).scrollTop();
+	this.previous_scroll = $(window).scrollTop();
 
 	$(window).on('scroll', function() {
 		var scroll = $(window).scrollTop(),
-			scroll_change = scroll - previous_scroll;
-		previous_scroll = scroll;
+			scroll_change = scroll - this.previous_scroll;
+		this.previous_scroll = scroll;
 		$(window).trigger('custom_scroll', [scroll_change]);
 	});
 
 	$(window).on('custom_scroll', function pos(e, scroll_change) {
 		console.log(scroll_change);
+		this.getScrollDest(scroll_change);
 	});
+}
+
+Deimos_Scroll.prototype.getScrollDest = function(scroll_change) {
+	if (this.previous_scroll < 1000) {
+		if (scroll_change > 0) {
+			this.scrollTo(1000);
+		} else {
+			this.scrollTo(0);
+		}
+	} else if (this.previous_scroll < 2000) {
+		if (scroll_change > 0) {
+			this.scrollTo(2000);
+		} else {
+			this.scrollTo(1000);
+		}
+
+	} else if (this.previous_scroll < 3000) {
+		if (scroll_change > 0) {
+			this.scrollTo(3000);
+		} else {
+			this.scrollTo(2000);
+		}
+	}
 }
