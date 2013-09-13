292b576ac660715bce8f6589e9ff943d75ff1cc7
Se Won Jang <swjang@stanford.edu>
2013-09-13 01:23:56 -0700

    sdfs

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Deimos::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-	root to: 'index#main' 
-
 	get '/' => 'index#main'
 
 	get '/support/index' => 'support#index'
